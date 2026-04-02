ad5c970c2f3d2054d874f56e3f77b327ae9fb3d7
Zac Valentine <zacv262@hotmail.com>
2026-04-01 23:41:54 -0400

    Added searchJobs and updateJob

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -63,11 +63,12 @@ router.delete("/deleteJob/:id", auth, async (req, res) => {
 // ------------------------
 router.post("/searchJobs", auth, async (req, res) => {
     try {
-        const { title, company } = req.body;
+        const { title, description, company } = req.body;
 
         // make filter
         const filter = {};
         filter.title = { $regex: title, $options: "i" };
+        filter.description = { $regex: description, $options: "i" };
         filter.company = { $regex: company, $options: "i" };
         
         // search with filter
@@ -80,5 +81,42 @@ router.post("/searchJobs", auth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// ------------------------
+// UPDATE JOB LISTING
+// ------------------------
+router.put("/updateJob/:id", auth, async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const createdBy = req.user.id; 
+    const { title, description, company } = req.body;
+
+    // retrieve job by id and check if it exists
+    const existingJob = await Job.findOne({ _id: jobId, createdBy });
+    if (!existingJob) {
+        return res.status(400).json({ message: "Job or permission not found" });
+    }
+    
+    // set job fields to new ones and save
+    if (title !== undefined) {
+        existingJob.title = title;
+    }
+    if (description !== undefined) {
+        existingJob.description = description;
+    }
+    if (company !== undefined) {
+        existingJob.company = company;
+    }
+    
+    await existingJob.save();
+
+    res.json({ message: "Job successfully updated", job: existingJob });
+
+  } catch (err) {
+    console.error("UPDATE JOB ERROR:", err);
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
