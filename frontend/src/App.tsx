2dfb389e715af550f8d730df71717c76aaf4586e
Nic <nicolejfm@gmail.com>
2026-04-13 14:33:40 -0400

    Add lazy loading for performance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Verify from "./pages/Verify";
-import Signup from "./pages/Signup";
-import ProfilePage from "./pages/ProfilePage";
-import JobSearchPage from "./pages/JobSearchPage";
-import EmployerProfile from "./pages/EmployerProfile";
-import ApplicantProfile from "./pages/ApplicantProfile";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
+import {lazy, Suspense} from "react"; //lazy loading for better performance
+
+const Login = lazy(() => import("./pages/Login"));
+const Verify = lazy(() => import("./pages/Verify"));
+const Signup = lazy(() => import("./pages/Signup"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const JobSearchPage = lazy(() => import("./pages/JobSearchPage"));
+const EmployerProfile = lazy(() => import("./pages/EmployerProfile"));
+const ApplicantProfile = lazy(() => import("./pages/ApplicantProfile"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword"));
+// import Login from "./pages/Login";
+// import Verify from "./pages/Verify";
+// import Signup from "./pages/Signup";
+// import ProfilePage from "./pages/ProfilePage";
+// import JobSearchPage from "./pages/JobSearchPage";
+// import EmployerProfile from "./pages/EmployerProfile";
+// import ApplicantProfile from "./pages/ApplicantProfile";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
     <Router>
+      <Suspense fallback={null}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -23,6 +35,7 @@ function App() {
         <Route path="/applicantProfile" element={<ApplicantProfile />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
+      </Suspense>
     </Router>
   );
 }
